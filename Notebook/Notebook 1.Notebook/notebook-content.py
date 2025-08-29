f6ad6f115bd3a55d58b8c8c672b9e9810816a0d1
BharathAjayGorli-hub <superonezero10@gmail.com>
2025-08-28 17:48:48 -0700

    Committing 2 items from workspace 51ff8cf4-d80c-4ed4-ac48-9afdf978c965

---
+++ Notebook/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,21 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "f2dd392f-2364-47c7-8490-f75b7bff8e53",
+# META       "default_lakehouse_name": "MigrationLakehouse",
+# META       "default_lakehouse_workspace_id": "51ff8cf4-d80c-4ed4-ac48-9afdf978c965",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "f2dd392f-2364-47c7-8490-f75b7bff8e53"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
